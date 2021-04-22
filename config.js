9012b2a28b6a1d3f832df79b810691e319ef1f40
Ray Messina <rmessina10@gmail.com>
2021-04-21 22:31:03 -0500

    Added mongoDB URL enviormental var to config file, for use with Atlas

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 module.exports = {
     'secretKey': process.env.SECRET_KEY,
-    'mongoUrl': 'mongodb://localhost:27017/kupboard'
+    'mongoUrl': process.env.MONGO_URL
 }
