0e244716b92e1acb690516be9b39d7cbd1dd80a2
rmessina1010 <rmessina10@gmail.com>
2025-07-02 00:29:57 -0500

    updates mongodb url logic in config

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
+const mongoUrl = process.env.RENDER === 'true' ?
+        "mongodb+srv://"+process.env.SECRET_USER+":"+process.env.SECRET_KEY+"@cluster0.acxx5xp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+        :'mongodb://localhost:27017/kupboard';
+
 module.exports = {
     'secretKey': process.env.SECRET_KEY,
-    'mongoUrl': 'mongodb://localhost:27017/kupboard'
+    'secretUser':process.env.SECRET_USER,
+    'mongoUrl': mongoUrl
 }
